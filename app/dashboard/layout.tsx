c310caec91d5f3e67d9c142d26b66961b9e6225e
g0ren <g0ren@proton.me>
2023-11-27 01:45:59 +0100

    basic style skeleton

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,29 +1,16 @@
 import Link from 'next/link';
+import NavLinks from '@/app/ui/components/navlinks';
+import Image from 'next/image';
 
 export default function DashboardLayout({
     children,
   }: {
     children: React.ReactNode;
   }) {
-    const groups = [
-      {
-        name: 'Banks',
-        sections: [
-          { name: 'Overview', href: '/dashboard/bankmain' },
-          { name: 'Accounts', href: '/dashboard/bankaccs'},
-          { name: 'Movements', href: '/dashboard/bankmovs'},
-          { name: 'Stocks', href: '/dashboard/bankstocks'},
-          { name: 'Funds', href: '/dashboard/bankfunds'},
-        ]
-      },
-      {
-        name: 'Exchanges',
-        sections: []
-      }
-    ];
     
-    return (
-        <div>
+    /*return (
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
             <h1><Link href="/dashboard">Dashboard Layout</Link></h1>
             <ul>{groups.map((group) => {
               return (
@@ -37,9 +24,39 @@ export default function DashboardLayout({
               );
             })}
             </ul>
-            <hr />
-            {children}
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+          </div>
         </div>
+    );*/
+
+    return (
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-64">
+          <div className="flex h-full flex-col px-3 py-4 md:px-2">
+            <Link
+              className="mb-2 flex h-20 items-end justify-start rounded-md bg-zinc-800 p-4 md:h-40"
+              href="/"
+            >
+                <Image
+                  src="/logo_original.png"
+                  width={192}
+                  height={192}
+                  alt="kaka"
+                  className="justify-center"
+                />
+            </Link>
+            <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+              <NavLinks />
+              <div className="hidden h-auto w-full grow rounded-md bg-zinc-700 md:block"></div>
+              <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-zinc-700 p-3 text-sm font-medium hover:bg-lime-100 hover:text-lime-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                {/* <PowerIcon className="w-6" /> */}
+                <div className="hidden md:block">Sign Out</div>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      </div>
     );
   }
   
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-zinc-800 text-zinc-100">
       <body>{children}</body>
     </html>
   );
