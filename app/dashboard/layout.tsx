083822c16197ab5788337a59a0ee0f014cc794cc
g0ren <g0ren@proton.me>
2023-11-27 18:38:57 +0100

    minor ui modification

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -32,19 +32,14 @@ export default function DashboardLayout({
     return (
       <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
         <div className="w-full flex-none md:w-64">
-          <div className="flex h-full flex-col px-3 py-4 md:px-2">
-            <Link
-              className="mb-2 flex h-20 items-end justify-start rounded-md bg-zinc-800 p-4 md:h-40"
-              href="/"
-            >
-                <Image
-                  src="/logo_original.png"
-                  width={192}
-                  height={192}
-                  alt="kaka"
-                  className="justify-center"
+          <div className="flex h-full flex-col px-3 py-2 md:px-2 item-center justify-center">
+            <Image
+                  src="/logo_dashboard.png"
+                  width={128}
+                  height={128}
+                  alt="Piggybank logo"
+                  className="ml-12 pb-2"
                 />
-            </Link>
             <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
               <NavLinks />
               <div className="hidden h-auto w-full grow rounded-md bg-zinc-700 md:block"></div>
