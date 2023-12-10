7fdafdbaf5214dcb4c649c9dfd58112002fc462f
g0ren <g0ren@proton.me>
2023-12-10 13:16:19 +0100

    add bank related type definitions

---
+++ app/lib/db/definitions.ts
@@ -0,0 +1,45 @@
+/**
+ * definitions.ts
+ * 
+ * File with database model types definitions
+ * 
+ */
+
+// Table: bank_accounts
+export type BankAccount = {
+    id: number;
+    name: string;
+    iban: string;
+    closed: string;
+    comments: string;
+}
+
+// Table: bank_movs
+export type BankMovs = {
+    id: number;
+    acc_id: number;
+    date: string;
+    category: number;
+    description: string;
+    value: number;
+    periodicity: number;
+    notes: string;
+}
+
+// Table: bank_stock_movs
+export type BankStocks = {
+    id: number;
+    acc_id: number;
+    date: string;
+    asset: number;
+    amount: number;
+}
+
+// Table: bank_fund_movs
+export type BankFunds = {
+    id: number;
+    acc_id: number;
+    date: string;
+    asset: number;
+    value: number;
+}
