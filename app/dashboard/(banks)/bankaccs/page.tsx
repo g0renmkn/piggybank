fea6f202c23a63523404a3028297e51e384478c2
g0ren <g0ren@proton.me>
2023-12-22 00:14:21 +0100

    add details element to hide form on accounts page

--- app/dashboard/(banks)/bankaccs/page.tsx
+++ app/dashboard/(banks)/bankaccs/page.tsx
@@ -7,8 +7,11 @@ import {
 export default async function Page() {
   return (
     <div>
-      <div className="flex flex-row"><BankIcon className="w-6 h-6 stroke-2" /><h1 className="text-lg px-2">Bank accounts</h1></div>
-      <AccountsForm />
+      <div className="flex flex-row"><BankIcon className="w-6 h-6 stroke-2 mb-6" /><h1 className="text-lg px-2">Bank accounts</h1></div>
+      <details>
+        <summary className="cursor-pointer">Create account</summary>
+        <AccountsForm />
+      </details>
       <AccountsTable />
     </div>
   );
