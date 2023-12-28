9839711e7c1e06b1a835b3e2cfc71ce34df9ffcb
g0ren <g0ren@proton.me>
2023-12-28 02:58:58 +0100

    add generic table component and use it for banks summary

--- app/dashboard/(banks)/bankmain/page.tsx
+++ app/dashboard/(banks)/bankmain/page.tsx
@@ -6,6 +6,7 @@
  */
 import { Card } from "@/app/ui/components/cards";
 import { banksSummary } from "@/app/lib/db/api_summaries";
+import { Table } from "@/app/ui/components/table";
 
 /**
  * <Page />
@@ -16,7 +17,6 @@ import { banksSummary } from "@/app/lib/db/api_summaries";
  */
 export default async function Page() {
     const result = await banksSummary();
-    console.log(result);
 
     const totalBalance = result.movs.total + result.stocks.total + result.funds.total;
 
@@ -26,36 +26,29 @@ export default async function Page() {
         <div className="grid grid-cols-3 gap-6">
 
           <div className="col-start-2">
-            <Card title="Total balance" value={totalBalance.toLocaleString("es-ES", { style: "currency", currency: "EUR" })} />
+            <Card title="Total balance" value={totalBalance.toLocaleString("de-DE", { style: "currency", currency: "EUR" })} />
           </div>
 
           <div className="flex flex-col gap-6 col-start-1">
-            <Card title="Cash" value={result.movs.total.toLocaleString("es-ES", { style: "currency", currency: "EUR" })} />
+            <Card title="Cash" value={result.movs.total.toLocaleString("de-DE", { style: "currency", currency: "EUR" })} />
             <Card title="Cash distribution chart" value="chart">
               <p>Chart Component</p>
             </Card>
-            <table>
-              <thead className="rounded-lg text-left text-sm font-normal">
-                <tr>
-                  <th scope="col" className="px-4 py-5 font-medium sm:pl-6">Account</th>
-                  <th scope="col" className="px-4 py-5 font-medium sm:pl-6">Value</th>
-                </tr>
-              </thead>
-              <tbody className="bg-zinc-700">
-                <tr>
-                  <td className="whitespace-nowrap px-3 py-3">BBVA Main</td>
-                  <td className="whitespace-nowrap px-3 py-3">2.345,23 €</td>
-                </tr>
-              </tbody>
-            </table>
+            <Table 
+              cols={["Account", "Balance"]} 
+              data={result.movs.accs.map((item:any) => [item.name, item.balance.toLocaleString("de-DE", {style: "currency", currency: "EUR"})])} 
+              />
           </div>
 
           <div className="flex flex-col gap-6">
-            <Card title="Stocks" value={result.stocks.total.toLocaleString("es-ES", { style: "currency", currency: "EUR" })} />
+            <Card title="Stocks" value={result.stocks.total.toLocaleString("de-DE", { style: "currency", currency: "EUR" })} />
             <Card title="Stocks distribution chart" value="chart">
               <p>Chart Component</p>
             </Card>
-            <p>Stocks distribution table</p>
+            <Table 
+              cols={["Account", "Balance"]} 
+              data={result.stocks.accs.map((item:any) => [item.name, item.balance.toLocaleString("de-DE", {style: "currency", currency: "EUR"})])} 
+              />
           </div>
 
           <div className="flex flex-col gap-6">
@@ -63,7 +56,10 @@ export default async function Page() {
             <Card title="Funds distribution chart" value="chart">
               <p>Chart Component</p>
             </Card>
-            <p>Funds distribution table</p>
+            <Table 
+              cols={["Account", "Balance"]} 
+              data={result.funds.accs.map((item:any) => [item.name, item.balance.toLocaleString("de-DE", {style: "currency", currency: "EUR"})])} 
+              />
           </div>
         </div>
       </div>
