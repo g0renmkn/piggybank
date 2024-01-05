022c8f51f4cf8d195dadb533ef9f5596dded0d6e
g0ren <g0ren@proton.me>
2024-01-05 02:19:05 +0100

    add fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Overlock, Genos, Josefin_Sans } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const overlock = Overlock({ subsets: ['latin'], weight: ["400", "700", "900"]})
+export const genos = Genos({ subsets: ['latin'], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]})
+export const josefin = Josefin_Sans({ subsets: ['latin'], weight: ["100", "200", "300", "400", "500", "600", "700"]})
