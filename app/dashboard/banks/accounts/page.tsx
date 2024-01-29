4374247a43e865aed289f20a8c9859e2c3dec3e4
g0ren <g0ren@proton.me>
2024-01-30 00:38:52 +0100

    add bank accounts page

--- app/dashboard/banks/accounts/page.tsx
+++ app/dashboard/banks/accounts/page.tsx
@@ -1,8 +1,74 @@
-export default function Page() {
+import { 
+    ContactPageOutlined,
+} from '@mui/icons-material';
+import { 
+    bankGetAccs,
+    bankCountAccs
+} from '@/app/data/api_banks_accounts';
+import Image from 'next/image';
+import {
+    Table,
+    THead,
+    Th,
+    TBody,
+    TRow,
+    Td,    
+} from '@/app/ui/components/table/table';
+
+
+export default async function Page(
+    {searchParams}: 
+    {
+        searchParams?: {
+            query?: string;
+            page?: string;
+            limit?: string;
+            dir?: string;
+            orderby?: string;
+        };
+    }
+) {
+    const currentLimit = Number(searchParams?.limit) || 10;
+    const accounts = await bankGetAccs(searchParams || {});
+    const accsCount = await bankCountAccs(searchParams || {});
+    const totalPages = Math.ceil(accsCount / currentLimit);
+
     return (
         <div>
-            This is the Bank Accounts page
+            <h1 className="text-lg font-bold pb-5"><ContactPageOutlined />Bank Accounts</h1>
+
+            <div className="flex flex-col items-center w-3/4 mx-auto">
+                <Table className="w-full" totalPages={totalPages}>
+                    <THead>
+                        <Th orderBy="id">ID</Th>
+                        <Th orderBy="name">Account</Th>
+                        <Th orderBy="iban">IBAN</Th>
+                        <Th>Comments</Th>
+                    </THead>
+                    <TBody>
+                    {
+                        accounts.map((acc: any) => {
+                            return (
+                                <TRow key={acc.id}>
+                                    <Td>{acc.id}</Td>
+                                    <Td>
+                                        <div className="h-full flex flex-row items-center">
+                                            <div className="w-[32px] h-[32px] relative rounded-full overflow-hidden mr-2">
+                                                <Image src={"/accounts/" + acc.pfp} alt="account logo" fill={true} />
+                                            </div>
+                                            {acc.name}
+                                        </div>
+                                    </Td>
+                                    <Td>{acc.iban}</Td>
+                                    <Td>{acc.comments}</Td>
+                                </TRow>
+                            );
+                        })
+                    }
+                    </TBody>
+                </Table>
+            </div>
         </div>
     );
-  }
+}
   
