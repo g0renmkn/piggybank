2abbde40f841a50b1f29e66261c13fcb6b0bd3ce
g0ren <g0ren@proton.me>
2024-01-30 00:37:56 +0100

    add api handler for bank account data

---
+++ app/data/api_banks_accounts.ts
@@ -0,0 +1,115 @@
+'use server';
+/**
+ * api_banks_accounts.ts
+ * 
+ * File that has all the functions for fetching data related to banks using the backend RESTful API.
+ * 
+ */
+import { unstable_noStore as noStore } from 'next/cache';
+import { BASE_URL } from '@/app/data/api_config';
+
+
+/**
+ * bankGetAccs()
+ * 
+ * Retrieve the accounts object from the database
+ * 
+ * @returns Accounts object from the DB
+ */
+export async function bankGetAccs(
+    {query, page, limit, dir, orderby}: 
+    {
+        query?: string;
+        page?: string;
+        limit?: string;
+        dir?: string;
+        orderby?: string;
+    }
+) {
+    noStore();
+    let url = BASE_URL + '/banks/accounts';
+    let querystr = [];
+
+    if (query) {
+        querystr.push("query="+query);
+    }
+    if (page) {
+        querystr.push("page="+page);
+    }
+    if (limit) {
+        querystr.push("limit="+limit);
+    }
+    if (dir) {
+        querystr.push("dir="+dir);
+    }
+    if (orderby) {
+        querystr.push("orderby="+orderby);
+    }
+
+    if (querystr.length>0) {
+        url += "?" + querystr.join("&");
+    }
+
+    const res = await fetch(url, {
+        next: {
+            revalidate: 10
+        }
+    });
+
+    let ret = [];
+    if( res.status === 200 ) {
+        ret = await res.json();
+    }
+    else {
+        let err = await res.json();
+        
+        throw {name: err.err, message: err.message};
+    }
+
+    return ret;
+}
+
+
+/**
+ * bankCountAccs()
+ * 
+ * Get the amount of banks/accounts
+ * 
+ * @returns An integer representing the row count of the desired query
+ */
+/**
+ * 
+ * @param endpoint 
+ * @param query
+ * @returns 
+ */
+export async function bankCountAccs(
+    {query}:
+    {query?: string}
+) {
+    noStore();
+    let url = BASE_URL + '/banks/accounts/count';
+    let querystr = [];
+
+    if (query) {
+        querystr.push("query="+query);
+    }
+
+    if (querystr.length>0) {
+        url += "?" + querystr.join("&");
+    }
+
+    const res = await fetch(url, {
+        next: {
+            revalidate: 10
+        }
+    });
+    let ret = 0;
+
+    if( res.status===200 ) { 
+        const resjson = await res.json();
+        ret = resjson.count;
+    }
+
+    return ret;
+}
---
+++ app/data/api_config.ts
@@ -0,0 +1,9 @@
+/**
+ * api_config.ts
+ * 
+ * API Configuration
+ * 
+ */
+
+const API_HOST = `${process.env.API_HOST}:${process.env.API_PORT}`
+export const BASE_URL = `http://${API_HOST}/api/v1`
